308f13902b42cfb9fc3258da2eae82e637114935
TheNameIsWin <ashwinvarma1105@gmail.com>
2026-02-06 14:15:52 +0530

    adding env file inside our project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { checkToken } = require("./middlewareToken");            //route level middleware
+require("dotenv").config();
 const app = express();
 
 app.use(express.json());                                        // we can use middleware more than one time
@@ -26,13 +27,13 @@ let myPass = 1212;
 // })
 //app.use(checkToken);    //middleware
 
-app.get('/', (req, res) => {
+app.get('/', checkToken, (req, res) => {
     res.send({ status: 1, message: "homepage" });
 });
 app.get('/about', checkToken, (req, res) => {
     res.send({ status: 1, message: "This is my about us page API" });
 });
-app.get('/products', (req, res) => {
+app.get('/products', checkToken, (req, res) => {
     res.send({ status: 1, msg: "this is my products page" });
 });
 app.get("/about/:id", (req, res) => {                           //here we're using param :id
@@ -57,6 +58,6 @@ app.post("/user", (req, res) => {
         queryData: req.query
     });
 });
-app.listen(4000, () => {
-    console.log("the server is running on port 4000 successfully") // it's working fine and showing message in terminal
+app.listen(process.env.PORT || 5000, () => {     //if we have port in env file then it will take that otherwise it will take 5000 as default port
+    console.log("the server is running on port " + process.env.PORT + " successfully") // it's working fine and showing message in terminal
 });  
